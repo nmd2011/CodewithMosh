c5c37cad2dde1189de71914853f5926d9f72cd3f
fyznitch <fyznitch@gmail.com>
2019-11-01 12:32:21 -0700

    First commit

---
+++ CodewithMosh/src/practice/mortgageCalculator.java
@@ -0,0 +1,17 @@
+package practice;
+
+import java.util.Scanner;
+
+public class mortgageCalculator {
+
+	public static void main(String[] args) {
+		final int months = 12;
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("What is the principal amount: ");
+		String principal = scanner.nextLine();
+		System.out.println(principal);
+
+	}
+
+}
